3ae7042fd90ce7f8700695cfb3b5f9e24a13d5bb
Nicholas Johnston <nicholas.johnston94@gmail.com>
2018-12-08 12:43:35 -0800

    created test portfoliocontainer class, having problems with npm

--- client/src/index.js
+++ client/src/index.js
@@ -1,18 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
 import './index.css';
 import Provider from 'react-dom';
 import PortfolioContainer from './containers/PortfolioContainer';
 import * as serviceWorker from './serviceWorker';
 
-const MyPortfolio = () => (
+const Portfolio = () => (
   <Provider>
     <PortfolioContainer />
   </Provider>
 );
 
-ReactDOM.render(<MyPortfolio />, document.getElementById('root'));
+ReactDOM.render(<Portfolio />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
