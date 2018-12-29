f215051eb0c3fa1d505d7abbf07fb75877cda3fd
Nicholas Johnston <nicholas.johnston94@gmail.com>
2018-12-28 22:17:45 -0800

    switched 3rd party slider dependency to material-ui dependency, familiar and more efficient

--- client/src/containers/PortfolioContainer/PortfolioContainer.js
+++ client/src/containers/PortfolioContainer/PortfolioContainer.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
 
+import SliderContainer from '../SliderContainer';
+
 class PortfolioContainer extends Component {
   render() {
     return (
       <div>
         <p>hello</p>
+        <div>
+          <SliderContainer />
+        </div>
       </div>
     );
   }
--- client/src/containers/SliderContainer/SliderContainer.js
+++ client/src/containers/SliderContainer/SliderContainer.js
@@ -1,41 +1,129 @@
 import React, { Component } from 'react';
-import Slider from 'react-slick';
+import PropTypes from 'prop-types';
+
+import { withStyles } from '@material-ui/core/styles';
+import Stepper from '@material-ui/core/Stepper';
+import Step from '@material-ui/core/Step';
+import StepLabel from '@material-ui/core/StepLabel';
+import StepContent from '@material-ui/core/StepContent';
+import Button from '@material-ui/core/Button';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
+
+const styles = (theme) => ({
+  root: {
+    width: '90%'
+  },
+  button: {
+    marginTop: theme.spacing.unit,
+    marginRight: theme.spacing.unit
+  },
+  actionsContainer: {
+    marginBottom: theme.spacing.unit * 2
+  },
+  resetContainer: {
+    padding: theme.spacing.unit * 3
+  }
+});
+
+function getSteps() {
+  return [ 'Select campaign settings', 'Create an ad group', 'Create an ad' ];
+}
+
+function getStepContent(step) {
+  switch (step) {
+    case 0:
+      return `For each ad campaign that you create, you can control how much
+              you're willing to spend on clicks and conversions, which networks
+              and geographical locations you want your ads to show on, and more.`;
+    case 1:
+      return 'An ad group contains one or more ads which target a shared set of keywords.';
+    case 2:
+      return `Try out different ad text to see what brings in the most customers,
+              and learn how to enhance your ads using features like ad extensions.
+              If you run into any problems with your ads, find out how to tell if
+              they're running and how to resolve approval issues.`;
+    default:
+      return 'Unknown step';
+  }
+}
 
 class SliderContainer extends Component {
+  state = {
+    activeStep: 0
+  };
+
+  handleNext = () => {
+    this.setState((state) => ({
+      activeStep: state.activeStep + 1
+    }));
+  };
+
+  handleBack = () => {
+    this.setState((state) => ({
+      activeStep: state.activeStep - 1
+    }));
+  };
+
+  handleReset = () => {
+    this.setState({
+      activeStep: 0
+    });
+  };
+
   render() {
-    const settings = {
-      dots: true,
-      infinite: true,
-      speed: 500,
-      slidesToShow: 1,
-      slidesToScroll: 1
-    };
+    const { classes } = this.props;
+    const steps = getSteps();
+    const { activeStep } = this.state;
+
     return (
-      <div>
-        <h2> Single Item</h2>
-        <Slider {...settings}>
-          <div>
-            <h3>1</h3>
-          </div>
-          <div>
-            <h3>2</h3>
-          </div>
-          <div>
-            <h3>3</h3>
-          </div>
-          <div>
-            <h3>4</h3>
-          </div>
-          <div>
-            <h3>5</h3>
-          </div>
-          <div>
-            <h3>6</h3>
-          </div>
-        </Slider>
+      <div className={classes.root}>
+        <Stepper activeStep={activeStep} orientation='vertical'>
+          {steps.map((label, index) => {
+            return (
+              <Step key={label}>
+                <StepLabel>{label}</StepLabel>
+                <StepContent>
+                  <Typography>{getStepContent(index)}</Typography>
+                  <div className={classes.actionsContainer}>
+                    <div>
+                      <Button
+                        disabled={activeStep === 0}
+                        onClick={this.handleBack}
+                        className={classes.button}
+                      >
+                        Back
+                      </Button>
+                      <Button
+                        variant='contained'
+                        color='primary'
+                        onClick={this.handleNext}
+                        className={classes.button}
+                      >
+                        {activeStep === steps.length - 1 ? 'Finish' : 'Next'}
+                      </Button>
+                    </div>
+                  </div>
+                </StepContent>
+              </Step>
+            );
+          })}
+        </Stepper>
+        {activeStep === steps.length && (
+          <Paper square elevation={0} className={classes.resetContainer}>
+            <Typography>All steps completed - you&quot;re finished</Typography>
+            <Button onClick={this.handleReset} className={classes.button}>
+              Reset
+            </Button>
+          </Paper>
+        )}
       </div>
     );
   }
 }
 
-export default SliderContainer;
+SliderContainer.propTypes = {
+  classes: PropTypes.object
+};
+
+export default withStyles(styles)(SliderContainer);
