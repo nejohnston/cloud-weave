b628272c25539b919eb36fca82adada9b4a3c97b
Nicholas Johnston <nicholas.johnston94@gmail.com>
2018-12-28 18:17:01 -0800

    added material-ui, added a button for test

--- client/src/components/HeaderBar/HeaderBar.js
+++ client/src/components/HeaderBar/HeaderBar.js
@@ -1,8 +1,12 @@
 import React from 'react';
 
+import Button from '@material-ui/core/Button';
+
 const HeaderBar = () => (
   <div>
-    <p>header</p>
+    <Button variant='contained' color='primary'>
+      Hello World
+    </Button>
   </div>
 );
 
