c8eb2f709253110eeff4337672fbe4cc523e6d42
Nicholas Johnston <nicholas.johnston94@gmail.com>
2019-01-20 11:34:25 -0800

    added menu icon to headerbar

--- client/src/components/HeaderBar/HeaderBar.js
+++ client/src/components/HeaderBar/HeaderBar.js
@@ -2,10 +2,15 @@ import React from 'react';
 
 import AppBar from '@material-ui/core/AppBar';
 import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
 
 const HeaderBar = () => (
   <div>
     <AppBar position='fixed'>
+      <IconButton aria-label='menu'>
+        <MenuIcon />
+      </IconButton>
       <Button variant='contained' color='primary'>
         Hello World
       </Button>
