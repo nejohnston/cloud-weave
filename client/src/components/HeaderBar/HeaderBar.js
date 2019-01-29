b7d6ed20a8b7b8830432854289c84205be5b4aeb
Nicholas Johnston <nicholas.johnston94@gmail.com>
2019-01-29 11:25:25 -0800

    attempting to add menu to header bar

--- client/src/components/HeaderBar/HeaderBar.js
+++ client/src/components/HeaderBar/HeaderBar.js
@@ -1,17 +1,27 @@
 import React from 'react';
 
 import MenuIcon from '@material-ui/icons/Menu';
-import { AppBar, IconButton, Menu, Toolbar, MenuItem } from '@material-ui/core';
+import {
+  AppBar,
+  IconButton,
+  Menu,
+  Toolbar,
+  MenuItem,
+  MenuList
+} from '@material-ui/core';
 
 const HeaderBar = () => (
   <div>
-    <AppBar position='sticky'>
+    <AppBar position='sticky' color='default'>
       <Toolbar>
         <IconButton aria-label='Menu'>
-          <Menu>
-            <MenuItem>hello</MenuItem>
-          </Menu>
+          <MenuIcon />
         </IconButton>
+        <Menu>
+          <MenuList>
+            <MenuItem>sdfdsfs</MenuItem>
+          </MenuList>
+        </Menu>
       </Toolbar>
     </AppBar>
   </div>
