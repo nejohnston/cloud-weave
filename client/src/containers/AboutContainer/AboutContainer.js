d460152f15777a377c46fb6f128059ae979e6612
Nicholas Johnston <nicholas.johnston94@gmail.com>
2019-01-30 12:45:32 -0800

    added import/export to /AboutContainer/index.js

---
+++ client/src/containers/AboutContainer/AboutContainer.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class AboutContainer extends Component {
+  render() {
+    return (
+      <div>
+        <p>about</p>
+      </div>
+    );
+  }
+}
+
+export default AboutContainer;
