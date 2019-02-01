c4069fdcbfcbcad4aaafdc39d097524969a98d2d
Nicholas Johnston <nicholas.johnston94@gmail.com>
2019-02-01 00:01:34 -0800

    using mui examples to work on tab routes

--- client/src/components/HeaderBar/HeaderBar.js
+++ client/src/components/HeaderBar/HeaderBar.js
@@ -1,6 +1,11 @@
 import React from 'react';
-
-import { AppBar, Toolbar, Tab, Tabs, Typography } from '@material-ui/core';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Tabs from '@material-ui/core/Tabs';
+import NoSsr from '@material-ui/core/NoSsr';
+import Tab from '@material-ui/core/Tab';
+import Typography from '@material-ui/core/Typography';
 
 function TabContainer(props) {
   return (
@@ -10,19 +15,61 @@ function TabContainer(props) {
   );
 }
 
-const HeaderBar = () => (
-  <div>
-    <AppBar position='sticky' color='default'>
-      <Toolbar>
-        <Tabs>
-          <Tab label='About' fullWidth='true' />
-          <Tab label='Projects' fullWidth='true' />
-        </Tabs>
-      </Toolbar>
-    </AppBar>
-    {value === 0 && <TabContainer>Item One</TabContainer>}
-    {value === 1 && <TabContainer>Item Two</TabContainer>}
-  </div>
-);
+TabContainer.propTypes = {
+  children: PropTypes.node.isRequired
+};
 
-export default HeaderBar;
+function LinkTab(props) {
+  return (
+    <Tab component='a' onClick={(event) => event.preventDefault()} {...props} />
+  );
+}
+
+const styles = (theme) => ({
+  root: {
+    flexGrow: 1,
+    backgroundColor: theme.palette.background.paper
+  }
+});
+
+class HeaderBar extends React.Component {
+  state = {
+    value: 0
+  };
+
+  handleChange = (event, value) => {
+    this.setState({ value });
+  };
+
+  render() {
+    const { classes } = this.props;
+    const { value } = this.state;
+
+    return (
+      <NoSsr>
+        <div className={classes.root}>
+          <AppBar position='static'>
+            <Tabs
+              variant='fullWidth'
+              value={value}
+              onChange={this.handleChange}
+            >
+              <LinkTab label='Page One' href='page1' />
+              <LinkTab label='Page Two' href='page2' />
+              <LinkTab label='Page Three' href='page3' />
+            </Tabs>
+          </AppBar>
+          {value === 0 && <TabContainer>Page One</TabContainer>}
+          {value === 1 && <TabContainer>Page Two</TabContainer>}
+          {value === 2 && <TabContainer>Page Three</TabContainer>}
+        </div>
+      </NoSsr>
+    );
+  }
+}
+
+NavTabs.propTypes = {
+  classes: PropTypes.object.isRequired
+};
+
+expor;
