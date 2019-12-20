ebce45d4d10e219483c2dc744f059f908ba97e82
Nicholas Johnston <nicholas.johnston94@gmail.com>
2019-12-19 21:48:46 -0800

    deleted yarn.lock for heroku deployment

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,22 +1,10 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import HeaderBar from '../HeaderBar';
 import ToTopButton from '../ToTopButton/ToTopButton';
 import { connect } from 'react-redux';
 
 import './styles.css';
 import SideBar from '../SideBar';
-
-const styles = (theme) => ({
-  root: {
-    position: 'relative'
-  },
-  fab: {
-    position: 'absolute',
-    bottom: theme.spacing.unit * 2,
-    right: theme.spacing.unit * 2
-  }
-});
 
 const Layout = ({ window, children }) => {
   return (
