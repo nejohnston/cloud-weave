449117897111cc8d6e955e0825242e4371c3a0a1
Nicholas Johnston <nicholas.johnston94@gmail.com>
2020-01-03 01:25:24 -0800

    updated headerbar with redux

--- src/components/HeaderBar/HeaderBar.js
+++ src/components/HeaderBar/HeaderBar.js
@@ -1,12 +1,13 @@
 import React from 'react';
 
-import { AppBar, Toolbar, Button } from '@material-ui/core';
+import { AppBar, Toolbar, Button, Avatar } from '@material-ui/core';
 import { Link } from 'react-scroll';
 
 const HeaderBar = () => (
   <div id='header'>
     <AppBar position='sticky' color='default'>
       <Toolbar>
+        <Avatar src='../../assets/images/me.jpg' />
         <Link to='about' spy={true} smooth={true} offset={-70} duration={500}>
           <Button>about</Button>
         </Link>
