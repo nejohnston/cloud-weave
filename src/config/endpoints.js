d103858e3c3ac7d20f7a3cf3546b34b036ff78c5
Nicholas Johnston <nicholas.johnston94@gmail.com>
2020-01-10 11:20:59 -0800

    added projects redux module

--- src/config/endpoints.js
+++ src/config/endpoints.js
@@ -1,5 +1,3 @@
 export const FIREBASE_URL = 'https://portfolio-c50b3.firebaseio.com/';
 
 export const PROJECTS_ENDPOINT = `${FIREBASE_URL}projects.json`;
-
-export const ABOUT_ENDPOINT = `${FIREBASE_URL}code_of_conduct.json`;
